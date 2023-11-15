78f9f5acec3aa2419fa2f82cf2f3017611912daf
Matthew Ismylove <matizmy235@gmail.com>
2023-11-16 03:22:06 +0600

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './css/App.css';
 import Registration from './components/Registration';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Registration />
+      <Routes>
+        <Route path="/registration" element={<Registration />}/> {/* 👈 Renders at /app/ */}
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}/> {/* 👈 Renders at /app/ */}
+      </Routes>
       </header>
     </div>
   );
