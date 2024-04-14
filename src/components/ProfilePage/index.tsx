66588ba4afd73156f08198e3f31b34afd311250a
Matvey Izmaylov <matizmy235@gmail.com>
2024-04-14 16:18:47 +0600

    in process of make-up

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Box, Flex, Text } from '@chakra-ui/react';
-import { Employee } from '../../type/dataType';
+import { Box, Flex, Text, Avatar, IconButton } from '@chakra-ui/react';
 import styles from './style.module.css';
 import classNames from 'classnames/bind';
+import { AddIcon } from '@chakra-ui/icons';
+import { FaRegBookmark } from 'react-icons/fa';
 
 const style = classNames.bind(styles);
 
@@ -16,7 +17,97 @@ function ProfilePage() {
         maxH="760px"
         h="100%"
         background="#D9D9D9"
-      ></Box>
+      >
+        <div className="profile__shortinfo">
+          <Flex justify="space-between" className={style('profile__personal')}>
+            <Avatar
+              size="xl"
+              name={'Иванов Иван'}
+              src="https://bit.ly/broken-link"
+            />
+            <Flex
+              className={style('employee-card__personal-info')}
+              direction="column"
+            >
+              <Text
+                className={style('employee-card__personal-info-name')}
+                fontSize="md"
+                fontWeight="500"
+              >
+                {'Иванов Иван Иванович'}
+              </Text>
+              <Text
+                className={style('employee-card__personal-info-spec')}
+                fontSize="md"
+              >
+                {'Frontend, middle'}
+              </Text>
+            </Flex>
+            <Flex
+              maxW="220px"
+              w="100%"
+              gap="20px"
+              className={style('employee-card__stats')}
+              color="black"
+            >
+              <Box
+                maxW="100px"
+                maxH="100px"
+                w="100%"
+                display="flex"
+                className={style('profile__hours')}
+                justifyContent="center"
+                alignItems="center"
+                borderRadius="16px"
+              >
+                {'4.5'}
+              </Box>
+              <Box
+                maxW="100px"
+                maxH="100px"
+                w="100%"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                backgroundColor="white"
+                borderRadius="16px"
+                border="4px solid gray"
+              >
+                {'5' + 'ч.'}
+              </Box>
+            </Flex>
+            <Flex direction="column" alignSelf="center" gap="40px">
+              <IconButton
+                aria-label="Some button"
+                size="s"
+                variant="unstyled"
+                color="black"
+                isRound={true}
+                alignSelf="center"
+                icon={
+                  <AddIcon
+                    background="white"
+                    padding="4px"
+                    borderRadius="50%"
+                    boxSize="24px"
+                  />
+                }
+                onClick={() => console.log('123')}
+              />
+              <IconButton
+                aria-label="Some button"
+                size="s"
+                variant="unstyled"
+                color="black"
+                isRound={true}
+                alignSelf="center"
+                icon={<FaRegBookmark />}
+                onClick={() => console.log('123')}
+              />
+            </Flex>
+          </Flex>
+        </div>
+      </Box>
     </div>
   );
 }
